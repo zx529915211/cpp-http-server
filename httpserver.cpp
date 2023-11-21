6372d48e05e6687835a522a7f92ecc2f8fd263f2
gzf <278299648@qq.com>
2023-11-21 22:45:30 +0800

    readme

--- httpserver.cpp
+++ httpserver.cpp
@@ -6,8 +6,8 @@ int main(int argc, char *argv[]) {
     //对端已经关闭了，服务端如果还在发送数据给客户段，则内核会发送SIGPIPE信号给进程，导致进程崩溃，这里忽略此信号
     signal(SIGPIPE, SIG_IGN);
     unsigned short port = 8000;
-    unsigned short port1 = 8888;
-    unsigned short port2 = 8889;
+//    unsigned short port1 = 8888;
+//    unsigned short port2 = 8889;
     if (argc > 1) {
         port = atoi(argv[1]);
     }
